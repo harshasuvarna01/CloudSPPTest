06b777c1f27b5fd63e69feffafe254bfa00b86c7
harshasuvarna01 <harsha.a.suvarna@gmail.com>
2019-04-23 16:20:55 +0530

    Added selenium jars to path and wrote code to open a URL in chrome

--- src/com/example/basics/login.java
+++ src/com/example/basics/login.java
@@ -1,10 +1,15 @@
 package com.example.basics;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class login 
 {
 	public static void main(String[] args) 
 	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
 		
-
+		driver.get("http://www.differencebetween.net/technology/difference-between-cvs-and-svn/");
+		driver.manage().window().maximize();
 	}
 }
