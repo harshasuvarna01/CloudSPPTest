b9703e020e2df2192bc7b3afb73db4db3ae326ab
harshasuvarna01 <harsha.a.suvarna@gmail.com>
2019-04-23 16:26:52 +0530

    added close browser at end of script

--- src/com/example/basics/login.java
+++ src/com/example/basics/login.java
@@ -4,12 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class login 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		
 		driver.get("http://www.differencebetween.net/technology/difference-between-cvs-and-svn/");
 		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		
+		driver.close();
 	}
 }
